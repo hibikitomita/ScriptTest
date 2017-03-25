27cd2c1742fae2ecb8591f600b1321a8e7263ca1
tomitahibiki <n4f0s8jmht9xkbxn2mhz@docomo.ne.jp>
2017-03-26 00:21:33 +0900

    ScriptTest

---
+++ Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        
+        int[] array = new int[5];
+        array[0] = 30;
+        array[1] = 20;
+        array[2] = 50;
+        array[3] = 10;
+        array[4] = 80;
+
+        for (int i = array.Length - 1; 0 <= i; i--)
+        {
+            Debug.Log(array[i]);
+        } 
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
